c11d48066b1ce19f9f6802c55711bed907a3bd99
Sem de Groot <semdegroot2003@gmail.com>
2025-10-03 16:49:45 +0200

    update webapp

--- core/models.py
+++ core/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Roster(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    file = models.FileField(upload_to="rooster/", blank=True, null=True)  # we schrijven zelf naar rooster/rooster.pdf
+    # Dit veld hoeft niet gebruikt te worden, maar laat 'm optioneel ivm bestaande rows:
+    file = models.FileField(upload_to="rooster/", blank=True, null=True)
 
     class Meta:
         permissions = [
@@ -13,6 +14,6 @@ class Roster(models.Model):
             ("can_access_availability",   "Mag Beschikbaarheid openen"),
             ("can_view_av_medications",   "Mag subtab Geneesmiddelen zien"),
             ("can_view_av_nazendingen",   "Mag subtab Nazendingen zien"),
-            ("can_view_news",             "Mag Nieuws bekijken"),
-            ("can_view_policies",         "Mag Werkafspraken bekijken"),
+            ("can_view_news",             "Mag Nieuws bekijken"),          # <— NIEUW
+            ("can_view_policies",         "Mag Werkafspraken bekijken"),   # <— NIEUW
         ]
