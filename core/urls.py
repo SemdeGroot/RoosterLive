512fd28a3665a9ed817fc52329901e69229a6360
Sem de Groot <semdegroot2003@gmail.com>
2025-10-05 09:42:01 +0200

    update paths

--- core/urls.py
+++ core/urls.py
@@ -7,16 +7,16 @@ urlpatterns = [
     path("upload/", views.upload_roster, name="upload_roster"),
 
     path("beschikbaarheid/", views.availability_home, name="availability_home"),
-    path("beschikbaarheid/geneesmiddelen/", views.availability_medications, name="availability_medications"),
+    path("beschikbaarheid/voorraad/", views.availability_medications, name="availability_medications"),
     path("beschikbaarheid/nazendingen/", views.availability_nazendingen, name="availability_nazendingen"),
 
     path("nieuws/", views.news, name="news"),
     path("werkafspraken/", views.policies, name="policies"),
 
-    path("adminx/", views.admin_panel, name="admin_panel"),
-    path("adminx/user/<int:user_id>/update/", views.user_update, name="user_update"),
-    path("adminx/user/<int:user_id>/delete/", views.user_delete, name="user_delete"),
-    path("adminx/group/<int:group_id>/delete/", views.group_delete, name="group_delete"),
+    path("beheer/", views.admin_panel, name="admin_panel"),
+    path("beheer/user/<int:user_id>/update/", views.user_update, name="user_update"),
+    path("beheer/user/<int:user_id>/delete/", views.user_delete, name="user_delete"),
+    path("beheer/group/<int:group_id>/delete/", views.group_delete, name="group_delete"),
 
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
