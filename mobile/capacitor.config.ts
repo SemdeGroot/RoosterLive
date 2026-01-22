825c93ddbf5bf1152edb0c504aaf426144eeea7f
SemdeGroot <semdegroot2003@gmail.com>
2026-01-22 11:40:27 +0100

    mobile updates not working yet but in progress

--- mobile/capacitor.config.ts
+++ mobile/capacitor.config.ts
@@ -1,28 +1,22 @@
 import type { CapacitorConfig } from '@capacitor/cli';
-import dotenv from 'dotenv';
-import path from 'node:path';
-
-dotenv.config({ path: path.resolve(__dirname, '../.env'), override: true });
-
-const isDebug = String(process.env.DEBUG || '').toLowerCase() === 'true';
-
-const DEV_URL = process.env.CAPACITOR_DEV_URL || 'https://app.apotheekjansen.com';
-const PROD_URL = process.env.CAPACITOR_PROD_URL || 'https://app.apotheekjansen.com';
-const TARGET_URL = isDebug ? DEV_URL : PROD_URL;
 
 const config: CapacitorConfig = {
   appId: 'com.apotheekjansen.app',
-  appName: 'Apotheek Jansen',
+  appName: 'Apo Jansen',
   webDir: 'www',
-  bundledWebRuntime: false,
   server: {
-    allowNavigation: [
-      'app.apotheekjansen.com',
-      '*.ngrok-free.dev',
-      'localhost',
-      '127.0.0.1',
-    ],
+    url: 'https://treasonably-noncerebral-samir.ngrok-free.dev',
+    cleartext: false
   },
+    plugins: {
+    SystemBars: {
+        insetsHandling: 'css', // injecteert --safe-area-inset-* in de WebView
+    },
+    StatusBar: {
+        // mag blijven, maar overlays heeft geen effect op Android 15+
+        overlaysWebView: true,
+    }
+    }
 };
 
 export default config;
