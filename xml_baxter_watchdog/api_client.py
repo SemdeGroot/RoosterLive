76d8f2e2111b57c30d55e2b7b7243c1af84274da
SemdeGroot <semdegroot2003@gmail.com>
2026-02-24 22:50:57 +0100

    fix bug API

--- xml_baxter_watchdog/api_client.py
+++ xml_baxter_watchdog/api_client.py
@@ -1,5 +1,5 @@
 import requests
-from xml_baxter_watchdog.env_config import API_URL, API_TOKEN
+from xml_baxter_watchdog.env_config import API_URL, API_TOKEN, PROXY_URL
 
 
 def stuur_naar_api(payload: dict) -> bool:
@@ -7,9 +7,10 @@ def stuur_naar_api(payload: dict) -> bool:
         "Content-Type": "application/json",
         "Authorization": f"Token {API_TOKEN}",
     }
+    proxies = {"http": PROXY_URL, "https": PROXY_URL} if PROXY_URL else None
 
     try:
-        response = requests.post(API_URL, json=payload, headers=headers, timeout=10)
+        response = requests.post(API_URL, json=payload, headers=headers, timeout=10, proxies=proxies)
         response.raise_for_status()
         return True
 
